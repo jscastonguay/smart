60c107243bf336334c17b55d20b1fdb49b41e77b
Jean-Sébastien Castonguay <jscastonguay@gmail.com>
2021-04-25 20:32:11 -0400

    Makefile avec cpp et gestion des dépendances (header files).

--- src/main.c
+++ src/main.c
@@ -1,6 +1,16 @@
 // Voici le lien pour réaliser un projet multi fichiers
 // https://dev.to/talhabalaj/setup-visual-studio-code-for-multi-file-c-projects-1jpi
 
+// Pour forcer le make à rebâtir si un header file est modifié:
+// https://stackoverflow.com/questions/52034997/how-to-make-makefile-recompile-when-a-header-file-is-changed
+
+// Un meilleur exemple afin de rebâtir lorsqu'un header file est modifié
+// https://www.cs.swarthmore.edu/~newhall/unixhelp/howto_makefiles.html
+
+// Un encore meilleur exemple afin de rabâtir si les header change. (pour le C et C++) En plus,
+// ça semble être une super doc sur les makefile
+// https://makefiletutorial.com/#makefile-cookbook
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -23,10 +33,9 @@ typedef struct {
 Individu individus[NB_INDIVIDUS];
 
 
-
 int main(int argc, char *argv[]) {
     
-    printf("Salut toto !!\n\r");
+    printf("Salut titi !!\n\r");
 
     return EXIT_SUCCESS;
 }
---
+++ src/test.cpp
@@ -0,0 +1,8 @@
+#include <test.h>
+#include <iostream>
+
+using namespace std;
+
+void Test::printString() {
+    cout << "printString de test";
+}
