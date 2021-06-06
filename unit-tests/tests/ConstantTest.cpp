a2dfcada9554c22122a59cffcdc2fd8367ded45d
Jean-Sébastien Castonguay <jscastonguay@gmail.com>
2021-06-06 11:32:03 -0400

    Durant le dév.

--- unit-tests/tests/ConstantTest.cpp
+++ unit-tests/tests/ConstantTest.cpp
@@ -1,4 +1,5 @@
 #include "CppUTest/TestHarness.h"
+#include "CppUTestExt/MockSupport.h"
 #include "Constant.h"
 
 extern "C" {
@@ -9,7 +10,10 @@ extern "C" {
 
 TEST_GROUP(Constant) {
     void setup(){}
-    void teardown(){}
+    void teardown(){
+        // TODO: À voir l'utilisation de MockSupportPlugin. Voir https://cpputest.github.io/mocking_manual.html
+        mock().clear();
+    }
 };
 
 TEST(Constant, Constructor) {
@@ -17,6 +21,11 @@ TEST(Constant, Constructor) {
     // Test basé sur le fait que la fonction srand(time(NULL))
     // n'a pas été appelée: donc la valeur retournée par rand() 
     // est toujours identique.
+    mock().expectOneCall("rand").andReturnValue(10);
+    mock().expectOneCall("rand").andReturnValue(10);
+    //mock().expectOneCall("rand").andReturnValue(10);
     Constant constant;
-    CHECK_EQUAL(-83.0, constant.getValue());
+    CHECK_EQUAL(-10, constant.getValue());
+
+    mock().checkExpectations();
 }
--- unit-tests/tests/OperationTest.cpp
+++ unit-tests/tests/OperationTest.cpp
@@ -1,4 +1,5 @@
 #include "CppUTest/TestHarness.h"
+#include "CppUTestExt/MockSupport.h"
 #include "Operation.h"
 
 extern "C" {
@@ -9,14 +10,16 @@ extern "C" {
 
 TEST_GROUP(Operation) {
     void setup(){}
-    void teardown(){}
+    void teardown() {
+        mock().clear();
+    }
 };
 
 TEST(Operation, Constructor) {
 
-    // Test basé sur le fait que la fonction srand(time(NULL))
-    // n'a pas été appelée: donc la valeur retournée par rand() 
-    // est toujours identique.
+    mock().expectOneCall("rand").andReturnValue(10);
+
     Operation operation;
     CHECK_EQUAL(0.0, operation.getValue());
+    mock().checkExpectations();
 }
